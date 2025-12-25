a8451e945f59d59466813a51e157053fd139a839
pushpendra patel <pushpendrapatel8055@gmail.com>
2025-12-25 11:52:07 +0530

    err fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-
+import './i18n.js';
 import { HeaderProvider } from './context/HeaderContext.jsx'
 
 createRoot(document.getElementById('root')).render(
